cf083a435761dca7e6d99704780af10f63dd2d3c
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-07-21 01:03:00 -0300

    aula8: restarting

--- terraform/ssm_parameter.tf
+++ terraform/ssm_parameter.tf
@@ -1,6 +1,5 @@
 module "test_parameter_store" {
-  # source = "git::https://github.com/therenanlira/container-arch--aws-modules.git//ssm_parameter_store?ref=v1"
-  source = "../../container-arch--aws-modules/ssm_parameter_store"
+  source = "git::https://github.com/therenanlira/container-arch--aws-modules.git//ssm_parameter_store?ref=v1"
 
   service_name = local.workspace.service_name
   value        = "abc123456"
--- terraform/ssm_secrets.tf
+++ terraform/ssm_secrets.tf
@@ -1,6 +1,5 @@
 module "test_secrets_manager" {
-  # source = "git::https://github.com/therenanlira/container-arch--aws-modules.git//ssm_parameter_store?ref=v1"
-  source = "../../container-arch--aws-modules/ssm_secrets_manager"
+  source = "git::https://github.com/therenanlira/container-arch--aws-modules.git//ssm_parameter_store?ref=v1"
 
   service_name = "${local.workspace.service_name}-test2"
   value        = "abc123456"
