2949ee24c95aad4830a3d5b1698e3ef62aba9d66
Renan Lira <22295448+therenanlira@users.noreply.github.com>
2026-08-11 17:39:50 -0300

    aula14

--- terraform/bastion.tf
+++ terraform/bastion.tf
@@ -4,6 +4,7 @@ module "bastion" {
   project_name   = local.workspace.project_name
   service_name   = "bastion"
   network_values = data.terraform_remote_state.aws_vpc.outputs
+  environment    = local.workspace.environment
 
   instance_type = "t4g.micro"
 }
